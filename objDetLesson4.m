932f4224633ef659ba4e4ef085b7c50f44a1c4a7
Pedro Bento <fc52823@alunos.fc.ul.pt>
2022-04-04 19:00:39 +0100

    iou made and some bugs fixed

--- objDetLesson4.m
+++ objDetLesson4.m
@@ -1,11 +1,12 @@
-
-
 clear all
 
 sampleXMLfile = 'PETS2009-S2L1.xml';
 mlStruct = parseXML(sampleXMLfile);
 
 imgbk = imread('View_001\\frame_0000.jpg');
+
+thresholdMatrix = (0:0.05:1);
+iouMatrix = zeros(1,length(thresholdMatrix));
 
 thr = 4;
 minArea = 200;
@@ -26,6 +27,8 @@ for i=1:seqLength
     imgfr = imread(sprintf('View_001\\frame_%.4d.jpg',baseNum+i));
     hold off
     subplot(2,2,1);imshow(imgfr);
+    
+    %Utilizar edge matrix para comparar só as edges de cada frame
     
     imgbkELR = imfilter(imgbk, edgeLR, 'conv');
     imgfrELR = imfilter(imgfr, edgeLR, 'conv');
@@ -51,6 +54,7 @@ for i=1:seqLength
     bwRL = imopen(imgdifRL,se);
     
     imgFinal = bwLR + bwRL;
+    %imgFinal = bw;
     
     subplot(2,2,1);imshow(imgbk);
     subplot(2,2,2);imshow(imgfr); 
@@ -62,35 +66,48 @@ for i=1:seqLength
     
     regnum = length(inds);
     genBoxes = zeros(regnum, 4);
+    areaSum = 0;
+    areaGTSum = 0;
 
     if regnum
         for j=1:regnum
             [lin, col]= find(lb == inds(j));
-            disp([lin, col]);
             upLPoint = min([lin col]);
             dWindow  = max([lin col]) - upLPoint + 1;
+            
+            %Guardar cada bounding box gerado pelo algoritmo numa matriz
             
             genBoxes(j, 1) = upLPoint(2);
             genBoxes(j, 2) = upLPoint(1);
             genBoxes(j, 3) = dWindow(2);
             genBoxes(j, 4) = dWindow(1);
+            %Soma das bounding boxes calculadas
+            
+            areaSum = areaSum + dWindow(1) * dWindow(2);
             
             rectangle('Position',[fliplr(upLPoint) fliplr(dWindow)],'EdgeColor',[1 1 0],...
                 'linewidth',2);
         end
     end
     
+    %Ler as bounding boxes do GT para este frame
+    
     a = size(mlStruct.Children((i+1)*2).Children(2).Children);
     a = int64((a(2)/2)-0.5);
-    disp(a);
+    %disp(a);
     bBoxes = zeros(a,4);
+    
+    %Guardar numa matriz
     
     for n = 1:a
         bBoxes(n,1) = str2double(mlStruct.Children((i+1)*2).Children(2).Children(n*2).Children(2).Attributes(1).Value);
         bBoxes(n,2) = str2double(mlStruct.Children((i+1)*2).Children(2).Children(n*2).Children(2).Attributes(2).Value);
         bBoxes(n,3) = str2double(mlStruct.Children((i+1)*2).Children(2).Children(n*2).Children(2).Attributes(3).Value);
         bBoxes(n,4) = str2double(mlStruct.Children((i+1)*2).Children(2).Children(n*2).Children(2).Attributes(4).Value);
+        areaGTSum = areaGTSum + bBoxes(n,1) * bBoxes(n,2);
     end
+    
+    %Converter para o formato bounding box
     
     for j=1:a
         upLPoint = [bBoxes(j, 3) - (bBoxes(j, 2)/2), bBoxes(j, 4) - (bBoxes(j, 1)/2)];
@@ -101,7 +118,26 @@ for i=1:seqLength
         bBoxes(j, 3) = dWindow(2);
         bBoxes(j, 4) = dWindow(1);
     end
-    drawnow
+    
+    area = rectint(bBoxes, genBoxes);
+    sumAreaIntersection = sum(sum(area));
+    
+    %disp(sumAreaIntersection);
+    %disp(areaSum);
+    %disp(areaGTSum);
+    
+    %Calcular a taxa de sucesso
+    
+    iou = sumAreaIntersection / (areaSum + areaGTSum - sumAreaIntersection);
+    
+    %Guardar para diferentes thresholds de modo a poder desenhar o gráfico
+    
+    for j=1:21
+        if iou > thresholdMatrix(1,j)
+            iouMatrix(1,j) = iouMatrix(1,j) + 1;
+        end
+    end
+    
 %     subplot(2,2,4);imshow(bw); drawnow
 %  
 %     [lb, num]=bwlabel(bw);
@@ -121,5 +157,8 @@ for i=1:seqLength
 %     end
 %     
 %     drawnow
+    disp(iouMatrix);
     
+    %Esta a criar um grafico para cada frame, mas pode se fazer so no fim
+    subplot(2,2,1); plot(thresholdMatrix, iouMatrix,'m--o'); drawnow
 end
